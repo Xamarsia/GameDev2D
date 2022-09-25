459c2b8d3af38c7e609182620e6faf76602ca341
xamarsia <xamarsia@gmail.com>
2022-09-26 01:01:29 +0200

    feat[player]: add flip

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,21 @@ public class PlayerMovement : MonoBehaviour
 
     private float _horizontal;
 
+    private bool _isFacingRight = true;
+
     void Update()
     {
         _horizontal = Input.GetAxisRaw("Horizontal");
         _rigidbody.velocity = new Vector2(_horizontal * _speed, _rigidbody.velocity.y);
+
+        Flip();
+    }
+    private void Flip()
+    {
+        if (_horizontal < 0 && _isFacingRight || _horizontal > 0 && !_isFacingRight)
+        {
+            _isFacingRight = !_isFacingRight;
+            transform.Rotate(0, 180, 0);
+        }
     }
 }
