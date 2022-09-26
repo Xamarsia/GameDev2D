8dc08ed2034706bc46a48148636fc5fb342f528b
xamarsia <xamarsia@gmail.com>
2022-09-26 03:34:51 +0200

    feat[player]: add and limit the jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,35 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody;
-    
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
+
     public float _speed = 4f;
+    public float _jumpingPower = 6f;
+    public float _groundCheckerRadius = 0.15f;
 
     private float _horizontal;
-
     private bool _isFacingRight = true;
 
     void Update()
     {
         _horizontal = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpingPower);
+        }
         _rigidbody.velocity = new Vector2(_horizontal * _speed, _rigidbody.velocity.y);
 
         Flip();
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(_groundCheck.position, _groundCheckerRadius);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(_groundCheck.position, _groundCheckerRadius, _groundLayer);
     }
     private void Flip()
     {
